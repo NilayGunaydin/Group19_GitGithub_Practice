4661012c9a89b22b65869623887ef7e6add38bbd
Cevriye Berkcan Turkmen <cberkcan@gmail.com>
2022-12-18 22:46:04 +0000

    3. sentence

--- src/test/CevriyeTurkmen.java
+++ src/test/CevriyeTurkmen.java
@@ -4,7 +4,7 @@ public class CevriyeTurkmen {
     public static void main(String[] args) {
         System.out.println("Hello my friends");
         System.out.println("Good luck with the new project");
-
+        System.out.println("Success to everyone");
 
 
 
