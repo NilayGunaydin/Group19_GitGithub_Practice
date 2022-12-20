794fce670fb38a79e9e77f9e4f2e80a33730c08a
Ishak Firat <iishakfirat@gmail.com>
2022-12-20 19:33:31 +0000

    updated TestIshak

--- src/test/featureİshak/TestIshak.java
+++ src/test/featureİshak/TestIshak.java
@@ -5,6 +5,8 @@ public class TestIshak {
     public static void main(String[] args) {
 
         System.out.println("Hello");
+        System.out.println("Good luck everyone for project");
+
 
 
     }
